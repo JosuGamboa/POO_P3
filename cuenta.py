0a33ae7990ee41e17964609f8055cb6c96d5b7e8
JosuGamboa <Josugamboafer70@gmail.com>
2023-10-31 17:52:31 -0400

    version 2023-10-31.03

--- cuenta.py
+++ cuenta.py
@@ -1,3 +1,10 @@
 class Cuenta:
-    def __init__(self):
+    def __init__(self, numero, titular, saldo, limite):
         print('Construyendo el objeto...{}'.format(self))
+        self.numero = numero
+        self.titular = titular
+        self.saldo = saldo
+        self.limite = limite
+        
+    def extracto(self):
+        print('Saldo {} del titular {}'.format(self.saldo, self.titular))
