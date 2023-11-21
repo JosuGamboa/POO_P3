8b81f5bf3339eefadce4fc35fd3af3e0fe6725be
JosuGamboa <Josugamboafer70@gmail.com>
2023-11-21 17:14:26 -0400

    Version 2023 - 11 - 21.02

--- cuenta.py
+++ cuenta.py
@@ -6,13 +6,17 @@ class Cuenta:
         self.__saldo = saldo
         self.__limite = limite
     def extracto(self):
-        print("saldo {} del titular {}  ".format(self.saldo,self.titular))
+        print("saldo {} del titular {}  ".format(self.__saldo, self.__titular))
     
     def depositar(self,valor):
         self.__saldo += valor
         
     def retirar (self,valor):
-        if(valor<= self.saldo):
-            self.__saldo -= valor
-        else:
-            print("No tiene suficiente saldo")
+        #if(valor<= self.saldo):
+        self.__saldo -= valor
+        #else:
+            #print("No tiene suficiente saldo")
+            
+    def transferir(self, valor, destino):
+        self.retirar(valor)
+        destino.depositar(valor)
